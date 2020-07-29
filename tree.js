77f6567e8d17d3560f34833093b8e6e8eb8aaef8
XavierCabeto <xaviercabeto@gmail.com>
2020-07-28 23:17:40 -0700

    first commit

---
+++ tree.js
@@ -0,0 +1,23 @@
+function Tree () {
+  this.root = null;
+}
+
+Tree.prototype.traverse = function () {
+  this.root.visit(this.root);
+}
+
+Tree.prototype.search = function (val) {
+  return this.root.search(val);
+}
+
+Tree.prototype.addValue = function (value) {
+  var n = new Node(value);
+
+  if (this.root == null) {
+    this.root = n;
+    this.root.x = width / 2;
+    this.root.y = 16;
+  } else {
+    this.root.addNode(n);
+  }
+}
